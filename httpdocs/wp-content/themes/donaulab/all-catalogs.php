a66a554bd8ee85f73e5f9f0c35fd9e987f4cee39
alex28742 <alex28731@gmail.com>
2019-01-19 21:35:01 +0300

    test commit

---
+++ httpdocs/wp-content/themes/donaulab/all-catalogs.php
@@ -0,0 +1,451 @@
+
+<?php
+/*
+ * Template Name: all-catalogs
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="ru-RU" prefix="og: http://ogp.me/ns#">
+<head profile="http://gmpg.org/xfn/11">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Dоnau Lаb Moscоw - Официальный сайт лабораторного оборудования в Москве</title>
+<link rel="shortcut icon" href="/images/favicon.ico">
+
+<meta name="description" content="Официальный сайт компании АО «Донау Лаб Москва». Российский представитель компании Donau Lab AG. Продажа лабораторного оборудования, комплексное оснащение лабораторий."/>
+<meta name="keywords" content="лабораторное оборудование, Донау Лаб, Donau Lab, официальный сайт"/>
+
+<?/*
+<link rel="stylesheet" type="text/css" href="/wp-content/themes/donaulab/style.css">
+<link rel="stylesheet" type="text/css" href="/wp-content/themes/donaulab/reset.css">
+<link rel="stylesheet" type="text/css" href="/wp-content/themes/donaulab/journal-styles.css">
+
+
+
+
+<link rel="stylesheet" id="rs-plugin-settings-css"  href="/wp-content/plugins/revslider/public/assets/css/settings.css" type="text/css" media="all">
+<link rel="stylesheet" id="wp-pagenavi-css"  href="/wp-content/plugins/wp-pagenavi/pagenavi-css.css" type="text/css" media="all">
+<link rel="stylesheet" id="fancybox-css"  href="/wp-content/plugins/easy-fancybox/fancybox/jquery.fancybox-1.3.7.css" type="text/css" media="screen">
+
+*/?>
+
+<!-- <script type="text/javascript" src="/js/jquery.js"></script> -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+<script type="text/javascript" src="http://donaulab.ru/wp-content/themes/donaulab/js/jquery.easing.1.3.js"></script>
+
+
+<!-- <script type="text/javascript" src="main.js"></script> -->
+<!-- <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script> -->
+<!-- <script src="//yastatic.net/share2/share.js"></script> -->
+<!-- Easy FancyBox 1.5.7 using FancyBox 1.3.7 - RavanH (http://status301.net/wordpress-plugins/easy-fancybox/) -->
+
+<!-- <script type='text/javascript' src='/wp-content/plugins/easy-fancybox/fancybox/jquery.fancybox-1.3.7.js'></script> -->
+<!-- <script type='text/javascript' src='/wp-content/plugins/easy-fancybox/jquery.easing.pack.js'></script> -->
+<!-- <script type='text/javascript' src='/wp-content/plugins/easy-fancybox/jquery.mousewheel.min.js'></script> -->
+
+
+
+
+
+
+
+<?/*
+<script type="text/javascript">
+jQuery(document).on('ready post-load', easy_fancybox_handler );
+</script>
+	<script type="text/javascript">
+		(function() {
+			var request, b = document.body, c = 'className', cs = 'customize-support', rcs = new RegExp('(^|\\s+)(no-)?'+cs+'(\\s+|$)');
+			request = true;
+			b[c] = b[c].replace( rcs, ' ' );
+			b[c] += ( window.postMessage && request ? ' ' : ' no-' ) + cs;
+		}());
+	</script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+
+*/?>
+
+</head>
+
+
+<script type="text/javascript">
+        function Animate2id(id,ease){ //the id to animate, the easing type
+    var animSpeed=2000; //set animation speed
+    var $container=$("#container"); //define the container to move
+    if(ease){ //check if ease variable is set
+        var easeType=ease;
+    } else {
+        var easeType="easeOutQuart"; //set default easing type
+    }
+    //do the animation
+    $container.stop().animate({"left": -($(id).position().left)}, animSpeed, easeType);
+}
+</script>
+
+<body class="home blog logged-in admin-bar no-customize-support">
+
+<div class="conteiner">
+
+    <!-- // Seminar content -->
+    <div class="seminar-content">
+
+<div class="sem_intro">
+
+  <div class="title">title</div>
+
+
+
+  <!-- <div class="sem_logo">
+    <a href="http://donaulab.ru"  target="_blank"><img src="http://donaulab.ru/wp-content/themes/donaulab/img/logo-1.jpg" alt="" /></a> &#160;    &#160;
+    <a href="https://www.buchi.com/ru-ru"  target="_blank"><img src="http://donaulab.ru/wp-content/themes/donaulab/img/logo-2.jpg" alt="" /></a>
+  </div> -->
+
+</div>
+
+
+<h1>h1</h1>
+
+<style>
+
+/*  */
+
+
+#menu{ z-index: 2; top:10px; left:10px; clear:both; border:1px solid #666; padding:5px; background:#fff;}
+#menu a{margin-left:5px; margin-right:5px;}
+#container{position:relative; left:0; width:10000px; top:0; height:100%;}
+#c1,#c2,#c3,#c4,#c5,#c6 {width:1600px; height:100%; float:left; margin-right:100px;}
+#c1,#c3,#c5 {background: #ffffff url(http://pcvector.net/templates/pcv/images/bg_grid.png);}
+#c2,#c4,#c6 {background:#B5E0FE;}
+.content{padding:80px 600px 20px 30px; }
+
+
+
+
+/*  */
+div.tabscontainer{
+    margin:15px 0px;
+}
+
+div.tabscontainer div.tabs{
+    list-style: none;
+    width: 260px;
+    cursor: pointer;
+    float:left;
+    margin-top: 10px;
+    left: 0px;
+    z-index: 2;
+}
+
+div.tabscontainer div.curvedContainer{
+	margin-left: 259px;
+	border:1px solid #7c7c77;
+	min-height:400px;
+	-moz-border-radius: 13px;
+	border-radius: 13px;
+}
+
+div.tabscontainer div.curvedContainer .tabcontent{
+	display:none;
+	padding:20px;
+	font-size:12px;
+	font-family: "CenturyGothicRegular", "Century Gothic", Arial, Helvetica, sans-serif;
+}
+
+div.tabs div.tab{
+    display: block;
+    height: 58px;
+    background: #eeeeea;
+    border: #d6d6d2 solid 1px;
+    border-top: none;
+    position: relative;
+   	color: #73736b;
+}
+
+div.tabs div.link{
+	padding-left: 20px;
+	padding-top:20px;
+	font-family: "CenturyGothicRegular", "Century Gothic", Arial, Helvetica, sans-serif;
+    font-size: 14px;
+}
+
+div.tabs div.tab.selected{
+    color: #ffffff;
+    border-right-color: #aeaeaa;
+}
+
+div.tabs div.tab.selected{
+    background: url(/wp-content/themes/donaulab/img/catalogs/menuSelBack.png) repeat-x;
+    border-right-color: #7c7c77;
+}
+
+
+
+div.tabs div.tab.first{
+	border-top: #dbdbb7 solid 1px;
+	-moz-border-radius-topleft: 13px;
+	border-top-left-radius: 13px;
+}
+
+div.tabs div.tab.last{
+	-moz-border-radius-bottomleft: 13px;
+	border-bottom-left-radius: 13px;
+}
+
+div.tabs div.tab div.arrow{
+    position: absolute;
+    background: url(/wp-content/themes/donaulab/img/catalogs/homeSelArrow.png) no-repeat;
+    height: 58px;
+    width: 17px;
+    left: 100%;
+    top: 0px;
+    display: none;
+}
+
+div.tabs div.tab.selected div.arrow{
+    display: block;
+}
+</style>
+
+
+
+
+<div class="tabscontainer">
+     <div class="tabs">
+         <div class="tab selected first" id="tab_menu_1">
+             <div class="link">Общие каталоги</div>
+             <div class="arrow"></div>
+         </div>
+         <div class="tab" id="tab_menu_2">
+             <div class="link">Каталоги по производителю</div>
+             <div class="arrow"></div>
+         </div>
+          <div class="tab last" id="tab_menu_3">
+             <div class="link">Каталоги по назначению</div>
+             <div class="arrow"></div>
+         </div>
+    </div>
+	<div class="curvedContainer">
+        <div class="tabcontent" id="tab_content_1" style="display:block">
+        <h1>Общие каталоги</h1>
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolor qui voluptates veritatis doloremque quas assumenda enim earum blanditiis dignissimos,
+        rerum ipsa, labore voluptas harum quaerat iusto repellendus ab saepe nisi. Soluta labore dolore, quo expedita officia ea facilis dolores quia aspernatur fuga
+        repellat, mollitia iusto
+        </div>
+
+
+
+        <div class="tabcontent" id="tab_content_2">
+        <h1>Каталоги по производителю</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolor qui voluptates veritatis doloremque quas assumenda enim earum blanditiis dignissimos,
+        rerum ipsa, labore voluptas harum quaerat iusto repellendus ab saepe nisi. Soluta labore dolore, quo expedita officia ea facilis dolores quia aspernatur fuga
+        repellat, mollitia iusto impedit, corrupti animi voluptatum dicta? Quia sit vel, at laboriosam ut officia quaerat, quae excepturi soluta facilis eos nisi nobis
+        nam neque a accusantium veniam aliquam. Incidunt accusamus quidem inventore, minus unde maxime. Eaque voluptatem id officiis, voluptatibus quos atque? Harum maiores
+        enim ex dicta. Ipsum nesciunt minus earum, laboriosam magnam, sequi illum officia similique vitae placeat voluptatem repellat in. Incidunt ex reiciendis architecto
+        obcaecati? Sed ea dignissimos illum voluptatum beatae necessitatibus aperiam debitis repellat modi porro natus nobis, rerum minima possimus! Dolores blanditiis omnis
+        nisi eius consectetur rem vitae eum,
+        quia similique voluptates, nulla reprehenderit eveniet quis quisquam dignissimos perferendis. Quasi, in expedita!
+        </div>
+
+
+		<div class="tabcontent" id="tab_content_3">
+        <h1>Каталоги по назначению</h1>
+        <div id="menu">
+        <a href="#" onClick="Animate2id('#c1'); return false" title="Перейти к содержанию 1-ого элемента. Тип easing: default">Контент 1</a>
+        <a href="#" onClick="Animate2id('#c2','easeInOutExpo'); return false" title="Перейти к содержанию 2-ого элемента.Тип easing: easeInOutExpo">Контент 2</a>
+        <a href="#" onClick="Animate2id('#c3','easeOutQuint'); return false" title="Перейти к содержанию 3-ого элемента. Тип easing: easeOutQuint">Контент 3</a>
+        <a href="#" onClick="Animate2id('#c4','easeOutElastic'); return false" title="Перейти к содержанию 4-ого элемента. Тип easing type: easeOutElastic">Контент 4</a>
+        <a href="#" onClick="Animate2id('#c5','easeOutBack'); return false" title="Перейти к содержанию 5-ого элемента. Тип easing type: easeOutBack">Контент 5</a>
+        <a href="#" onClick="Animate2id('#c6','easeOutBounce'); return false" title="Перейти к содержанию 6-ого элемента. Тип easing type: easeOutBounce">Контент 6</a>
+        </div>
+
+        <div id="container">
+
+        <div id="c1">
+            <div class="content">Стивен Пол Джобс, более известный как Стив Джобс (англ. Steven Paul Jobs, Steve Jobs; род. 24 февраля 1955) — американский инженер и предприниматель, сооснователь и генеральный директор американской корпорации Apple Inc. Джобс также является основателем и бывшим генеральным директором студии Pixar.
+            <br />
+            <div align="center"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg/225px-Steve_Jobs_Headshot_2010-CROP.jpg"  alt="Стив Джобс"/></div>
+            <br/>
+        В конце 1970-х годов Джобс с соучредителем компании Apple Стивом Возняком создали один из первых коммерчески успешных персональных компьютеров. В начале 1980-х годов Джобс был в числе первых людей, обративших внимание на коммерческий потенциал компьютерной мыши при использовании её в графическом интерфейсе пользователя.
+        </div>
+        </div>
+
+
+        <div id="c2">
+            <div class="content">Ли́нус Бенедикт То́рвальдс, или Турвальдс (швед. Linus Benedict Torvalds [ˈliːnɵs ˈtuːrvalds]; 28 декабря 1969, Хельсинки, Финляндия) — финский программист, хакер.
+
+        <br />
+        <div align="center"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Linus_Torvalds.jpeg/200px-Linus_Torvalds.jpeg" alt="Линус Торвальдс"/></div>
+        </div>
+        </div>
+
+        <div id="c3">
+            <div class="content">
+            Уи́льям Ге́нри Гейтс III (англ. William Henry Gates III; 28 октября 1955, Сиэтл), более известный как просто Билл Гейтс (англ. Bill Gates) — американский
+            <br />
+        <div align="center"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Bill_Gates_World_Economic_Forum_2007.jpg/200px-Bill_Gates_World_Economic_Forum_2007.jpg" alt="Билл Гейтс"/></div>
+        <br />
+        Билл Гейтс является одним из рекордсменов по размеру средств, переданных на благотворительность: в период с 1994 по 2010 г. он вложил в Фонд Билла и Мелинды Гейтс более $28 млрд. В феврале 2010 года Гейтс выступил с предложением ко всем миллиардерам о передаче половины их состояния на благотворительную деятельность.
+            </div>
+        </div>
+
+
+
+
+        <div id="c4">
+            <div class="content">Го́рдон Эрл Му́р (англ. Gordon Earle Moore; 3 января 1929, Сан-Франциско, Калифорния) — почётный председатель совета директоров и основатель корпорации Intel, основоположник «закона Мура».
+                <br />
+        <div align="center"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Gordon_Moore.jpg/200px-Gordon_Moore.jpg" alt="Гордон Мур"/></div>
+        <br />
+        С 1953 года работал в лаборатории прикладной физики в Johns Hopkins University. С 1956 г. работал в Shockley Semiconductor Laboratory в Пало Альто, Калифорния, под руководством Вильяма Шокли(William Shockley).
+
+            </div>
+        </div>
+
+
+
+
+        <div id="c5">
+            <div class="content">Брюс Уи́ллис, урождённый Уо́лтер Брюс Уи́ллис (англ. Walter Bruce Willis, род. 19 марта 1955, Идар-Оберштайн, ФРГ) — американский киноактёр, продюсер,
+            <br />
+        <div align="center"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Bruce_Willis_by_Gage_Skidmore.jpg/200px-Bruce_Willis_by_Gage_Skidmore.jpg" alt="Брюс Уиллис"/></div>
+            </div>
+        </div>
+
+        <div id="c6">
+            <div class="content">Контент 6</div>
+        </div>
+
+
+
+        </div><!--end container-->
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+	</div>
+</div>
+
+<div id="sem-zayavka">
+<?php echo do_shortcode('[contact-form-7 id="11441" title="Семинар (webinar-buchi)"]'); ?>
+</div>
+
+
+
+
+
+<div class="sem-footer">
+  <a href="http://donaulab.ru">Перейти на корпоративный сайт "Донау Лаб. Москва"</a>
+</div>
+
+
+    </div>
+    <!-- // Seminar content -->
+
+</div><!-- .conteiner // -->
+
+
+<style>
+/* .seminar-content {padding: 20px 0;  text-align: center;}
+.seminar-content h1 {font-weight: 700; font-size: 36px;}
+.seminar-content p {padding: 0 0 20px 0; margin: 0;}
+.sem_intro {padding: 0 0 20px 0; margin: 0 0 30px 0; border-bottom: 5px solid #cccccc;}
+.sem_intro .title {font-size: 30px; padding: 0 0 20px 0; }
+.sem_intro .sem_logo img {border: 1px solid #cccccc;}
+
+.doklad_tab {width: 100%; margin: 0 0 30px 0;}
+.doklad_tab td {text-align: left; border-bottom: 1px solid #e6e6e6; padding: 20px 10px 0 10px; vertical-align: top;}
+.doklad_tab td b {display: block; font-size: 1.125em; text-transform: uppercase;}
+.bttn {
+  display: inline-block; height: 54px; line-height: 54px; color: #ffffff; text-decoration: none; text-transform: uppercase; font-size: 20px; font-weight: 700; padding: 0 20px; border: 0; border-radius: 10px; cursor: pointer;
+background: #4593cb;
+  background: -moz-linear-gradient(top, #4593cb 0%, #016bb7 100%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#4593cb), color-stop(100%,#016bb7));
+  background: -webkit-linear-gradient(top, #4593cb 0%,#016bb7 100%);
+  background: -o-linear-gradient(top, #4593cb 0%,#016bb7 100%);
+  background: -ms-linear-gradient(top, #4593cb 0%,#016bb7 100%);
+  background: linear-gradient(to bottom, #4593cb 0%,#016bb7 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#4593cb', endColorstr='#016bb7',GradientType=0 );
+}
+.bttn:hover {background: #016bb7; color: #ffffff;}
+.bttn_brdr {background: #ffffff; padding: 0 20px; font-size: 1.125em; height: 40px; line-height: 38px; text-transform: uppercase;}
+
+
+form#sem-zayavka {text-align: left; margin: 0 auto 20px auto; width: 335px; color: #666666;}
+form#sem-zayavka input[type=text],
+form#sem-zayavka input[type=email] {width: 100%; text-align: left; padding: 0 10px; height: 50px; line-height: 50px; border-radius: 5px; border: 1px solid #cccccc;}
+form#sem-zayavka input[type=text].error,
+form#sem-zayavka input[type=email].error {border: 1px solid red !important;}
+.img_line {padding: 20px 0; border-top: 1px solid #e6e6e6;}
+.img_line img {margin: 0 10px; border: 7px solid #e6e6e6;}
+.sem_autor { background: url(http://donaulab.ru/wp-content/themes/donaulab/img/bg.jpg) 50% 50% no-repeat; background-size: cover; padding: 30px 0;}
+.table-box.autor_tab {width: auto; margin: 0 auto 30px auto;}
+.table-box.autor_tab > div {vertical-align: middle; padding: 0 10px; text-align: left;}
+.table-box.autor_tab > div.img span {display: block; border-radius: 50%; overflow: hidden; border: 7px solid #e6e6e6;}
+.table-box.autor_tab > div.img span img {display: block; border-radius: 50%;margin: 0;}
+.table-box.autor_tab > div.txt .title {font-weight: 700; font-size: 1.5em; line-height: 100%; text-transform: uppercase;}
+.table-box.autor_tab > div.txt p {padding: 0 0 5px 0; font-size: 1.25em;}
+
+.sem-footer {font-size: 0.875em; padding: 20px 0 0 0; border-top: 1px solid #cccccc;}
+.small {font-style: italic; font-size: 0.875em;}
+.small input { margin: 0 10px 0 0; vertical-align: middle;}
+.img_line a {display: inline-block; vertical-align: middle; width: 28.5%; margin: 0 1%;}
+.img_line a img {width: 100%; height: auto;}
+.bttn {background: #9eff00;}
+.bttn:hover {background: #94d32e;}
+
+
+
+@media screen and (max-width: 1030px) {
+.img_line img {width: 150px; height: auto;}
+}
+@media screen and (max-width: 500px) {
+.sem_intro .sem_logo img {display: block; margin: 0 auto 20px auto;}
+.doklad_tab td img {width: 70px; height: auto;}
+form#sem-zayavka { padding: 0 10px;  width: 100%;}
+.table-box.autor_tab,
+.table-box.autor_tab > div {display: block; padding: 0 0 20px 0; text-align: center;}
+.table-box.autor_tab > div.img span {width: 250px; margin: 0 auto;}
+.table-box.autor_tab > div.img img {width: 100%; height: auto;}
+} */
+
+</style>
+
+
+
+<script>
+$(document).ready(function() {
+
+    $(".tabs .tab[id^=tab_menu]").hover(function() {
+        var curMenu=$(this);
+        $(".tabs .tab[id^=tab_menu]").removeClass("selected");
+        curMenu.addClass("selected");
+
+        var index=curMenu.attr("id").split("tab_menu_")[1];
+        $(".curvedContainer .tabcontent").css("display","none");
+        $(".curvedContainer #tab_content_"+index).css("display","block");
+    });
+
+
+
+
+
+
+
+
+});
+</script>
+
+
+
+</body>
+</html>
